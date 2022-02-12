8e78ff014765e266b34fd2c3e0e45d1f656387e6
ilkan Kızılkaya <87038864+kizilkaya-ilkan@users.noreply.github.com>
2022-02-12 13:11:45 +0300

    Add files via upload

---
+++ Cryptology/Cryptology/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cryptology
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sezar = textBox1.Text;
+            int key = Convert.ToInt32(textBox2.Text);
+            char[] karekterler = sezar.ToCharArray();
+            foreach (char eleman in karekterler)
+            {
+               textBox3.Text += Convert.ToChar(eleman + key).ToString();
+          
+            }
+            MessageBox.Show(textBox3.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sezar = textBox1.Text;
+            int key = Convert.ToInt32(textBox2.Text);
+            char[] karekterler = sezar.ToCharArray();
+            foreach (char eleman in karekterler)
+            {
+                textBox3.Text += Convert.ToChar(eleman - key).ToString();
+            }
+            MessageBox.Show(textBox3.Text);
+        }
+    }
+}
